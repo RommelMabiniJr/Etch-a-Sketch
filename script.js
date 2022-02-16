d0b7273918b4c2e364645bec55886ac6bfb9c51e
Rommel B. Mabini Jr <rommelbrillantesmabinijr@gmail.com>
2022-02-16 16:14:19 +0800

    Add hover effect using eventlisteners with random color applied

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const gridContr = document.querySelector('.grid-container');
+makeGrid(16, 16);
+
+let sqrBoxes = document.querySelectorAll('.square-div');
+
+
+
+sqrBoxes.forEach(box => {
+    box.addEventListener('mouseover', function(e) {
+        colorToAssign = createColor();
+        e.target.style.backgroundColor = colorToAssign;
+
+
+        setTimeout(function() {
+            e.target.style.backgroundColor = "";
+          }, 500);
+    })
+})
 
 function makeGrid(rows, col) {
     gridContr.style.setProperty('--grid-rows', rows);
@@ -10,4 +27,14 @@ function makeGrid(rows, col) {
     }
 }
 
-makeGrid(16, 16);
+function createColor() {
+    let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f']
+    let ranColor = '#';
+
+    for (let x = 0; x < 6; x++) {
+        let ranElement = Math.floor(Math.random() * 16);
+        ranColor += arr[ranElement];
+    }
+    
+    return ranColor;
+}
